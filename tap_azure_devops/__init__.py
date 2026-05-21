427235293754bcb3b0ef58515acb61ba37bd6889
Leyu <lambert@bilanc.co>
2026-05-21 16:17:52 +0100

    use : as delimiter in pr_id

--- tap_azure_devops/__init__.py
+++ tap_azure_devops/__init__.py
@@ -636,7 +636,7 @@ def get_all_pull_requests(schemas, repo_path, state, mdata, start_date):
             return
         seen_pr_numbers.add(pr_number)
 
-        pr["id"] = f"{project}-{repo_name}-{pr_number}"
+        pr["id"] = f"{project}:{repo_name}:{pr_number}"
         pr["pr_number"] = pr_number
         pr["repo_url"] = pr.get("repository", {}).get("remoteUrl")
         pr["_sdc_repository"] = repo_path
